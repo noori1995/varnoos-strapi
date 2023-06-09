59a4b08b57d2098f3bf1908dec8f4733c5a8ac88
noori <mohamad10.noori@gmail.com>
2023-06-09 16:46:10 +0330

    add mail server

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,28 @@
 const crypto = require("crypto");
 
 module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        secure: true,
+      },
+      settings: {
+        defaultFrom: "mohamad10.noori@gmail.com",
+        defaultReplyTo: "mohamad10.noori@gmail.com",
+      },
+    },
+  },
   "users-permissions": {
     config: {
       jwtSecret: env("JWT_SECRET") || crypto.randomBytes(16).toString("base64"),
     },
   },
 });
+
